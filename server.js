b2f2b94cb56b8ac409f4af3a6b3d95791bdaf3f9
Stas Dudzik <stas.dudzik@gmail.com>
2023-07-10 11:32:47 +0200

    write a server

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const packageDef = protoLoader.loadSync("./todo.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
-const todoPackage = grpc.todoPackage;
+const todoPackage = grpcObject.todoPackage;
+
+const server = new grpc.Server();
+// make server listen, server needs credentials, we can bypass it for testing purpose
+server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure());
+server.addService(todoPackage.Todo.service, {
+  createTodo: createTodo,
+  readTodos: readTodos,
+});
+server.start();
+
+function createTodo(call, callback) {
+  console.log(call);
+}
+
+function readTodos(call, callback) {}
